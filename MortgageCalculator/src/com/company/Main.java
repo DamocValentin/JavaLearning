a33c3c4c6981ddf6bf0e4e73743709f59f2c2e97
DamocValentin <valentin.damoc@yahoo.com>
2019-12-11 18:33:41 +0200

    Added conditions for input

--- MortgageCalculator/src/com/company/Main.java
+++ MortgageCalculator/src/com/company/Main.java
@@ -10,16 +10,37 @@ public class Main {
         final byte PERCENT = 100;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
+        int principal = 0;
+        float monthlyInterest = 0;
+        int numberOfPayments = 0;
 
-        System.out.print("Annual interest rate: ");
-        float annualInterest = scanner.nextFloat();
-        float monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+        while (true) {
+            System.out.print("Principal: ");
+            principal = scanner.nextInt();
+            if (principal >= 1000 && principal <= 1_000_000)
+                break;
+            System.out.println("Enter a value between 1000 and 1000000");
+        }
 
-        System.out.print("Period (years): ");
-        byte years = scanner.nextByte();
-        int numberOfPayments = years * MONTHS_IN_YEAR;
+        while (true) {
+            System.out.print("Annual interest rate: ");
+            float annualInterest = scanner.nextFloat();
+            if (annualInterest >= 1 && annualInterest <= 30){
+                monthlyInterest = annualInterest / PERCENT / MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
+
+        while (true) {
+            System.out.print("Period (years): ");
+            byte years = scanner.nextByte();
+            if( years >=1 && years <= 30){
+                numberOfPayments = years * MONTHS_IN_YEAR;
+                break;
+            }
+            System.out.println("Enter a value between 1 and 30");
+        }
 
         double temporaryPow = Math.pow( 1 + monthlyInterest, numberOfPayments);
         double mortgage = (principal *  monthlyInterest * temporaryPow) / (temporaryPow - 1);
